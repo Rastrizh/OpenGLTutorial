0ce5e475719719f10a429448b944e39f42117e49
Александр Растриженков <a.rastrizhenkov@gmail.com>
2021-02-04 16:35:09 +0300

    GLFW window

--- OpenGLTutorial/src/Main.cpp
+++ OpenGLTutorial/src/Main.cpp
@@ -1,7 +1,57 @@
 #include "glad\glad.h"
 #include "GLFW\glfw3.h"
 
+const unsigned int WIDTH = 1024;
+const unsigned int HIGHT = 768;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int hight)
+{
+	glViewport(0, 0, width, hight);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
 int main()
 {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(1024, 768, "OpenGLTurorial", nullptr, nullptr);
+	if (!window)
+	{
+		// Failed to create GLFW Window
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		// Failed to initialize GLAD
+		return -1;
+	}
+
+	glViewport(0, 0, 1024, 768);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		processInput(window);
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
