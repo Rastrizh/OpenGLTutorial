c6f88ad3c70464f7da86a6278edbe3f01c902d31
Александр Растриженков <a.rastrizhenkov@gmail.com>
2021-02-16 09:42:06 +0300

    rotated cubes (correctly)

--- OpenGLTutorial/src/Main.cpp
+++ OpenGLTutorial/src/Main.cpp
@@ -111,7 +111,7 @@ int main()
 	};
 
 	glm::vec3 cubePositions[] = {
-		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(0.1f,  0.1f,  -1.0f),
 		glm::vec3(2.0f,  5.0f, -15.0f),
 		glm::vec3(-1.5f, -2.2f, -2.5f),
 		glm::vec3(-3.8f, -2.0f, -12.3f),
@@ -216,15 +216,15 @@ int main()
 
 		glBindVertexArray(VAO);
 
-		glm::mat4 model = glm::mat4(1.0f);
+		//glm::mat4 model = glm::mat4(1.0f);
 		glm::mat4 view = glm::mat4(1.0f);
 		glm::mat4 projection = glm::mat4(1.0f);
 
-		model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+		//model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 		projection = glm::perspective(glm::radians(45.0f), (float)WIDTH / (float)HIGHT, 0.1f, 100.0f);
 
-		ourShader.setMat4("model", model);
+		//ourShader.setMat4("model", model);
 		ourShader.setMat4("view", view);
 		ourShader.setMat4("projection", projection);
 
@@ -234,7 +234,7 @@ int main()
 		{
 			glm::mat4 model = glm::mat4(1.0f);
 			model = glm::translate(model, cubePositions[i]);
-			float angle = 20.0f * i *glfwGetTime();
+			float angle = 20.0f * (i+1) *glfwGetTime();
 
 			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
 			ourShader.setMat4("model", model);
