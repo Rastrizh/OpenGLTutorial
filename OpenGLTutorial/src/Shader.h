9f9f3288480b8e3b7781e6b3b083a89451813c67
Александр Растриженков <a.rastrizhenkov@gmail.com>
2021-04-23 11:46:08 +0300

    Change std::string to char*

--- OpenGLTutorial/src/Shader.h
+++ OpenGLTutorial/src/Shader.h
@@ -67,24 +67,24 @@ public:
 	
 	void inline Use() { glUseProgram(ID); }
 
-	void inline SetBool(const std::string &name, bool value) const	{ glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value); }
-	void inline SetInt(const std::string &name, int value) const { glUniform1i(glGetUniformLocation(ID, name.c_str()), value); }
-	void inline SetFloat(const std::string &name, float value) const { glUniform1f(glGetUniformLocation(ID, name.c_str()), value); }
-	// ------------------------------------------------------------------------
-	void inline setVec2(const std::string& name, const glm::vec2& value) const	{ glUniform2fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]); }
-	void inline setVec2(const std::string& name, float x, float y) const { glUniform2f(glGetUniformLocation(ID, name.c_str()), x, y); }
-	// ------------------------------------------------------------------------
-	void inline setVec3(const std::string& name, const glm::vec3& value) const { glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]); }
-	void inline setVec3(const std::string& name, float x, float y, float z) const { glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z); }
-	// ------------------------------------------------------------------------
-	void inline setVec4(const std::string& name, const glm::vec4& value) const { glUniform4fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]); }
-	void inline setVec4(const std::string& name, float x, float y, float z, float w) const { glUniform4f(glGetUniformLocation(ID, name.c_str()), x, y, z, w); }
-	// ------------------------------------------------------------------------
-	void inline setMat2(const std::string& name, const glm::mat2& mat) const { glUniformMatrix2fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]); }
-	// ------------------------------------------------------------------------
-	void inline setMat3(const std::string& name, const glm::mat3& mat) const { glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]); }
-	// ------------------------------------------------------------------------
-	void inline setMat4(const std::string& name, const glm::mat4& mat) const { glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]); }
+	void inline SetBool (const char* name, bool value) const	{ glUniform1i(glGetUniformLocation(ID, name), (int)value); }
+	void inline SetInt  (const char* name, int value) const { glUniform1i(glGetUniformLocation(ID, name), value); }
+	void inline SetFloat(const char* name, float value) const { glUniform1f(glGetUniformLocation(ID, name), value); }
+	// ------------------------------------------------------------------------------------------------
+	void inline setVec2 (const char* name, const glm::vec2& value) const	{ glUniform2fv(glGetUniformLocation(ID, name), 1, &value[0]); }
+	void inline setVec2 (const char* name, float x, float y) const { glUniform2f(glGetUniformLocation(ID, name), x, y); }
+	// ---------------- -----------------------------------------------------------------------------------
+	void inline setVec3 (const char* name, const glm::vec3& value) const { glUniform3fv(glGetUniformLocation(ID, name), 1, &value[0]); }
+	void inline setVec3 (const char* name, float x, float y, float z) const { glUniform3f(glGetUniformLocation(ID, name), x, y, z); }
+	// ---------------- -----------------------------------------------------------------------------------------
+	void inline setVec4 (const char* name, const glm::vec4& value) const { glUniform4fv(glGetUniformLocation(ID, name), 1, &value[0]); }
+	void inline setVec4 (const char* name, float x, float y, float z, float w) const { glUniform4f(glGetUniformLocation(ID, name), x, y, z, w); }
+	// ---------------- ----------------------------------------------------------------------------------------------
+	void inline setMat2 (const char* name, const glm::mat2& mat) const { glUniformMatrix2fv(glGetUniformLocation(ID, name), 1, GL_FALSE, &mat[0][0]); }
+	// ---------------- ----------------------------------------------------------------------------------------------------
+	void inline setMat3 (const char* name, const glm::mat3& mat) const { glUniformMatrix3fv(glGetUniformLocation(ID, name), 1, GL_FALSE, &mat[0][0]); }
+	// ---------------- ---------------------------------------------------------------------------------------------------------
+	void inline setMat4 (const char* name, const glm::mat4& mat) const { glUniformMatrix4fv(glGetUniformLocation(ID, name), 1, GL_FALSE, &mat[0][0]); }
 
 private:
 	void checkCompileErrors(unsigned int shader, std::string type)
